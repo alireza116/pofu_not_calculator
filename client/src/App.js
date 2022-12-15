a86a35598e889106b10979ff032228d983ff32fd
Alireza Karduni <alireza116@gmail.com>
2022-12-15 11:39:23 -0600

    added basic icon array

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import IconArray from "./components/IconArray";
 import "./App.css";
 
 function App() {
@@ -7,6 +8,9 @@ function App() {
     const { data } = await axios.get(`/getThings`);
     console.log(data);
   };
+
+  const [numCols, setNumCols] = useState(10);
+  const [numRows, setNumRows] = useState(10);
 
   useEffect(() => {
     getData();
@@ -17,6 +21,27 @@ function App() {
       <header className="text-3xl font-bold underline">
         <p>IDEO website for Label Maker</p>
       </header>
+      <hr></hr>
+      <input
+        type="range"
+        min={5}
+        max={20}
+        onChange={(event) => {
+          setNumCols(parseInt(event.target.value));
+        }}
+      />
+      <input
+        type="range"
+        min={5}
+        max={20}
+        onChange={(event) => {
+          setNumRows(parseInt(event.target.value));
+        }}
+      />
+      <div>{numCols * numRows}</div>
+      <div className="mt-20" style={{ width: "600px", height: "600px" }}>
+        <IconArray numCols={numCols} numRows={numRows}></IconArray>
+      </div>
     </div>
   );
 }
