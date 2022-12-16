13e9230ee56b12c1e244e9761d3da05fef5fe945
nullboundary <archive.of.receipts@gmail.com>
2022-12-16 15:41:03 -0800

    improve styling and make calc drive visual

--- client/src/components/flightco2e.jsx
+++ client/src/components/flightco2e.jsx
@@ -1,28 +1,57 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-function FlightCo2e() {
+function FlightCo2e({ state }) {
+  const {
+    flightDistance,
+    setFlightDistance,
+    distanceUnit,
+    setDistanceUnit,
+    co2e,
+    setCo2e,
+    co2eUnit,
+    setCo2eUnit,
+    countIcon,
+    setCountIcon,
+    visualUnit,
+    setVisualUnit,
+  } = state;
+
+  const metricEqv = {
+    oil: 0.0023,
+    trees: 0.017,
+  };
+
+  const ImperialEqv = {
+    oil: 0.0011,
+    trees: 0.008,
+  };
+
   const getFlightCo2e = async (distance, unit) => {
     const { data } = await axios.get(`/getFlightCo2e`, { params: { distance: distance, unit: unit } });
     console.log(data);
-    setCo2e(data.value.toFixed(2));
+    const dataValue = data.value.toFixed(2);
+    setCo2e(dataValue);
     setCo2eUnit(data.unit);
+    if (unit === 'km') {
+      console.log(dataValue * metricEqv[visualUnit]);
+      setCountIcon((dataValue * metricEqv[visualUnit]).toFixed(1));
+    } else {
+      setCountIcon((dataValue * ImperialEqv[visualUnit]).toFixed(1));
+    }
   };
-
-  const [flightDistance, setFlightDistance] = useState(0);
-  const [distanceUnit, setDistanceUnit] = useState('km');
-  const [co2e, setCo2e] = useState(0);
-  const [co2eUnit, setCo2eUnit] = useState(0);
 
   useEffect(() => {
     getFlightCo2e(flightDistance, distanceUnit);
-  }, [flightDistance, distanceUnit]);
+  }, [flightDistance, distanceUnit, visualUnit]);
 
   return (
-    <div>
-      <label for='flightDistance'>Flight Distance </label>
+    <div className='pt-10 flex flex-wrap'>
+      <label for='flightDistance' className="text-left font-['Arima'] text-4xl leading-snug text-gray-200">
+        Well, how far are you flying?
+      </label>
       <input
-        className='border-2'
+        className="appearance-none bg-transparent border-b border-gray-200 font-['Arima'] text-4xl text-gray-200 w-1/3 mr-3 py-1 px-2 leading-tight focus:outline-none"
         type='number'
         name='flightDistance'
         onChange={(event) => {
@@ -30,6 +59,7 @@ function FlightCo2e() {
         }}
       />
       <select
+        className="block appearance-none  w-1/4 bg-transparent border-b border-gray-200 hover:border-gray-200 px-4 py-2 pr-8 leading-tight focus:outline-none focus:shadow-outline font-['Arima'] text-4xl text-gray-200"
         name='unit'
         id='distance-unit-select'
         onChange={(event) => {
@@ -38,8 +68,19 @@ function FlightCo2e() {
         <option value='km'>Km</option>
         <option value='miles'>Miles</option>
       </select>
-      <div className='font-bold'>
-        {co2e} {co2eUnit} Co2 per passenger
+      <h1 className="pt-12 font-['Arima'] text-4xl text-gray-200">.</h1>
+      <div className='w-full flex flex-wrap'>
+        <h1 className="text-left pt-12 font-['Arima'] text-4xl text-gray-200">How would you like to visual it?</h1>
+        <select
+          className="block w-3/5 appearance-none bg-transparent border-b border-gray-200 hover:border-gray-200 pt-10 pr-8 leading-tight focus:outline-none focus:shadow-outline font-['Arima'] text-4xl text-gray-200"
+          name='unit'
+          id='distance-unit-select'
+          onChange={(event) => {
+            setVisualUnit(event.target.value);
+          }}>
+          <option value='oil'>Barrels of Oil</option>
+          <option value='trees'>Trees To Offset</option>
+        </select>
       </div>
     </div>
   );
--- client/src/pages/LabelMaker.js
+++ client/src/pages/LabelMaker.js
@@ -1,43 +1,81 @@
-import React, { useEffect, useState, Suspense, useRef } from "react";
-import IdeoLogo from "../assets/ideo-logo.svg";
-import { ArrowDown, ArrowLeft } from "phosphor-react";
-import { Link } from "react-router-dom";
-import Fade from "react-reveal/Fade";
-import IconArrayByCount from "../components/IconArrayByCount";
+import React, { useEffect, useState, Suspense, useRef } from 'react';
+import IdeoLogo from '../assets/ideo-logo.svg';
+import { ArrowDown, ArrowLeft } from 'phosphor-react';
+import { Link } from 'react-router-dom';
+import Fade from 'react-reveal/Fade';
+import IconArrayByCount from '../components/IconArrayByCount';
+import FlightCo2e from '../components/flightco2e';
 
 function LabelMaker() {
   // const contentRef = useRef(null)
   // const scrollToContent = () => contentRef.current.scrollIntoView({ behavior: 'smooth' })
-  const [countIcon, setCountIcon] = useState(55);
+  const [countIcon, setCountIcon] = useState(0);
   const [iconSize, setIconSize] = useState(100);
   const [pct, setPct] = useState(100);
+
+  const [flightDistance, setFlightDistance] = useState(0);
+  const [distanceUnit, setDistanceUnit] = useState('km');
+  const [co2e, setCo2e] = useState(0);
+  const [co2eUnit, setCo2eUnit] = useState(0);
+  const [visualUnit, setVisualUnit] = useState('oil');
+
+  const flightCalcState = {
+    flightDistance,
+    setFlightDistance,
+    distanceUnit,
+    setDistanceUnit,
+    co2e,
+    setCo2e,
+    co2eUnit,
+    setCo2eUnit,
+    countIcon,
+    setCountIcon,
+    visualUnit,
+    setVisualUnit,
+  };
+
+  const iconPath = {
+    oil: '/images/oilbarrel.svg',
+    trees: '/images/tree.svg',
+  };
+
+  const bgColor = {
+    oil: 'bg-yellow-400',
+    trees: 'bg-green-400',
+  };
+
+  const getDescription = () => {
+    switch (visualUnit) {
+      case 'oil':
+        return `Or is like burning ${countIcon} barrels of oil each!`;
+      case 'trees':
+        return `Or can be offset by growing ${countIcon} trees for ten years!`;
+      default:
+        return '';
+    }
+  };
+
   return (
     <>
-      <main className="h-screen flex">
-        <div className="flex-1 bg-zinc-900 p-24">
-          <Link
-            className="mx-5 text-left font-['Arima'] flex mx-0 text-2xl text-white"
-            to="/home"
-          >
-            <ArrowLeft size={28} className="px-0 text-white" />
+      <main className='h-screen flex'>
+        <div className='flex-1 bg-zinc-900 p-24'>
+          <Link className="mx-5 text-left font-['Arima'] flex mx-0 text-2xl text-white" to='/home'>
+            <ArrowLeft size={28} className='px-0 text-white' />
             &nbsp;Back
           </Link>
-          <h1 className="text-left font-['Arima'] text-4xl leading-snug mt-10 text-white">
-            Make your Label HERE
-          </h1>
-          <div>
-            <label
-              htmlFor="count"
-              className="text-left block text-lg font-medium text-gray-200 "
-            >
+
+          <h1 className="text-left font-['Arima'] text-4xl leading-snug mt-10 text-gray-200">What if I flew there?</h1>
+          <FlightCo2e state={flightCalcState} />
+          {/* <div>
+            <label htmlFor='count' className='text-left block text-lg font-medium text-gray-200 '>
               How many icons?
             </label>
-            <div className="mt-1">
+            <div className='mt-1'>
               <input
-                type="number"
-                name="count"
-                id="email"
-                className="block w-1/3 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm h-12 p-1"
+                type='number'
+                name='count'
+                id='email'
+                className='block w-1/3 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm h-12 p-1'
                 placeholder={countIcon}
                 onChange={(event) => {
                   setCountIcon(parseInt(event.target.value));
@@ -46,49 +84,42 @@ function LabelMaker() {
             </div>
           </div>
           <div>
-            <label
-              htmlFor="size"
-              className="text-left block text-lg font-medium text-gray-200 "
-            >
+            <label htmlFor='size' className='text-left block text-lg font-medium text-gray-200 '>
               What should be the icon size?
             </label>
-            <div className="mt-1">
+            <div className='mt-1'>
               <input
-                type="range"
-                name="count"
+                type='range'
+                name='count'
                 min={10}
                 max={100}
                 default={50}
-                className="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm h-12 p-1"
+                className='w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm h-12 p-1'
                 placeholder={countIcon}
                 onChange={(event) => {
                   setIconSize(parseInt(event.target.value));
                 }}
               />
             </div>
-          </div>
+          </div> */}
         </div>
-        <div className="flex-1 overflow-y-scroll p-12">
-          {/* <div className="bg-amber-200">
-                        // label area
-                    </div> */}
-
-          <div className="bg-yellow-400 pt-20 flex-1 p-10">
+        <div className='flex-1 overflow-y-scroll p-12'>
+          <div className={`${bgColor[visualUnit]} pt-20 flex-1 p-10`}>
             <h1 className="text-left font-['Arima'] ml-3 mb-8 text-4xl">
-              Flying {1000} miles
+              Flying {flightDistance} {distanceUnit}
             </h1>
-            <h1 className="text-left font-['Arima'] ml-3 mb-8 text-4xl">
-              Is like using {countIcon} barrels of oil
+            <h1 className="text-left font-['Arima'] text-2xl leading-snug text-gray-400 ml-3">
+              is {co2e} {co2eUnit} of Co2 per passenger
             </h1>
+            <h1 className="text-left font-['Arima'] ml-3 mb-8 text-4xl">{getDescription()}</h1>
             <IconArrayByCount
               count={countIcon}
               iconSize={iconSize}
-              icon={"/images/oilbarrel.svg"}
+              icon={iconPath[visualUnit]}
               pct={pct}
               showHighlightFirst={false}
-              primaryColor="#202020"
-              secondaryColor="#fafafa"
-            ></IconArrayByCount>
+              primaryColor='#202020'
+              secondaryColor='#fafafa'></IconArrayByCount>
           </div>
         </div>
       </main>
